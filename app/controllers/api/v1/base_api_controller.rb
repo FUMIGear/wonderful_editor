738e95f19840b9916a2ff5dd71510e1327c962ed
FUMIGear <iso.fumi429@gmail.com>
2023-08-26 16:10:15 +0900

    Task7-2の模範回答を確認しました。 ・api/v1ディレクトリを作成 ・base_api_controller.rbファイルを作成し、クラス継承を行なった。

---
+++ app/controllers/api/v1/base_api_controller.rb
@@ -0,0 +1,5 @@
+# class BaseApiController < ApplicationController
+# end
+# 模範回答
+class Api::V1::BaseApiController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   resources :sample_articles
   # resources :sample_articles, :article # この方法でもいいが、sample_articleは後で消すから分けた。
-  resources :article, path: '/api/v1/articles'
-  mount_devise_token_auth_for "User", at: "auth"
+  # resources :article, path: '/api/v1/articles'
+  # mount_devise_token_auth_for "User", at: "/api/v1/auth" #path:とat:は同じ意味かも
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # 模範回答
+  # Task7-1：模範回答
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for "User", at: "auth"
-      resources :articles
+      mount_devise_token_auth_for "User", at: "auth" #Task5-3で自動追加
+      resources :articles # Task7-1で追加
     end
   end
 end
